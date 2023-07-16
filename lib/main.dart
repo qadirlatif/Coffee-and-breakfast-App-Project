9d117cc3c5c8d45d7760b3613c332ab9a97ff467
qadirlatif <aq456789.ab@gmail.com>
2023-07-16 14:59:23 +0500

    added Splash Screen, Discover Screen and Preloader

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,33 @@
+
 import 'package:flutter/material.dart';
+//import 'bottomBar.dart';
+import 'splash_screen/splashscree.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+   const MyApp({super.key});
+  
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green
+        //colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 32, 238, 39)),
+        //useMaterial3: true,
       ),
+      home: const SplashScreen(),
+      routes: {
+        //'/home': (context) => const BottomBar(),
+        //'/Coffee': (context) => const Coffee(),
+        
+      },
     );
   }
 }
